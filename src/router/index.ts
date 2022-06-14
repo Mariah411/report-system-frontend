d84a15710a5041cacd8ef53164158541d52c4850
Maria <mashakh04@gmail.com>
2022-06-14 22:11:34 +0300

    Correct routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,27 @@
-
 import EventPage from "../pages/EventPage";
 import FirstPage from "../pages/FirstPage";
 import Login from "../pages/Login";
+import ProgrammsPage from "../pages/ProgrammsPage";
 import SettingsPage from "../pages/SettingsPage";
 
 import TasksPage from "../pages/TasksPage";
 
 // интерфейс маршрута
 export interface IRoute {
-    path: string;
-    element: React.ComponentType;
-    index: boolean
+  path: string;
+  element: React.ComponentType;
+  index: boolean;
 }
 
 //перечисление маршрутов
-export enum RouteNames{
-    LOGIN = '/login',
-    FIRST = '/', 
-    TASKS = 'tasks',
-    USER_ID = '/user/:id',
-    PROGRAMMS = '/programms',
-    EVENTS = '/events',
-    SETTINGS = 'settings'
+export enum RouteNames {
+  LOGIN = "/login",
+  FIRST = "/",
+  TASKS = "tasks",
+  USER_ID = "/user/:id",
+  PROGRAMMS = "/programms",
+  EVENTS = "/events",
+  SETTINGS = "settings",
 }
 
 /*//публичные маршруты
@@ -41,15 +41,14 @@ export const privateRoutes: IRoute[] = [
 ]*/
 
 export const publicRoutes: IRoute[] = [
-    {path: RouteNames.LOGIN, element: Login, index: false},
-
-]
+  { path: RouteNames.LOGIN, element: Login, index: false },
+];
 
 //приватные маршруты
 export const privateRoutes: IRoute[] = [
-    {path: RouteNames.FIRST, element: FirstPage, index: true },
-    {path: RouteNames.TASKS, element: TasksPage, index: false},
-    {path: RouteNames.SETTINGS, element: SettingsPage, index: false},
-    {path: RouteNames.EVENTS, element: EventPage, index: false},
-    {path: RouteNames.PROGRAMMS, element: EventPage, index: false}
-]
+  { path: RouteNames.FIRST, element: FirstPage, index: true },
+  { path: RouteNames.TASKS, element: TasksPage, index: false },
+  { path: RouteNames.SETTINGS, element: SettingsPage, index: false },
+  { path: RouteNames.EVENTS, element: EventPage, index: false },
+  { path: RouteNames.PROGRAMMS, element: ProgrammsPage, index: false },
+];
